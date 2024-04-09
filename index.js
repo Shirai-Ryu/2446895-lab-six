a6d6bdc60c62a84dc1446d67235510ca8142275a
Shirai-Ryu <104224002+Shirai-Ryu@users.noreply.github.com>
2024-04-09 21:00:08 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+//create cars api using express
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const cars = require('./cars.json');
+
+// Initialize each car with an id if not present
+cars.forEach(car => {
+    car.id = car.id || require('crypto').randomBytes(4).toString('hex');
+});
+
+// [rest of the API routes here, no change needed]
+
+
+app.use(express.json());
+
+const cars = require('./cars.json');
+
+//get all cars
+app.get('/cars', (req, res) => {
+    res.json(cars);
+});
+
+//get car by id
+app.get('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const car = cars.find(car => car.id === id);
+    res.json(car);
+});
+
+//update car
+app.put('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedCar = req.body;
+    const index = cars.findIndex(car => car.id === id);
+    cars[index] = updatedCar;
+    res.json(updatedCar);
+});
+
+//delete car
+app.delete('/cars/:id', (req, res) => {
+    const id = req.params.id;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    res.json({ message: `Car with id ${id} deleted` });
+});
+
+//add car
+app.post('/cars', (req, res) => {
+    console.log(req);
+    const newCar = req.body;
+    console.log(newCar);
+    cars.push(newCar);
+    res.json(newCar);
+});
+
+//start app at localhost:3001
+app.listen(3001, () => {
+    console.log('Server started at http://localhost:3001');
+});
+
